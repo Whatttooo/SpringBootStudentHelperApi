2ab7513f145378e227ecefc9ed99d193f7690e47
whatttooo <j.whateley@hotmail.co.nz>
2017-10-16 11:38:39 +1300

    Completed Student Entity and added Apache Derby embedded database at runtime

--- src/main/java/com/whateley/CourseApiApplication.java
+++ src/main/java/com/whateley/CourseApiApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseApiApplication {
 
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiApplication.class, args);
 		System.out.println("Listening on http://localhost:8080");
